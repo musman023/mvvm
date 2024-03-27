509f123036f3cb512926d621ff6e3d1f00f90cec
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-27 17:57:55 +0500

    set routes names

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mvvmapp/utils/routes/routes_name.dart';
+import 'package:mvvmapp/view/home_screen.dart';
+import 'package:mvvmapp/view/login_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.home:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => const HomeScreen());
+      case RoutesName.login:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => const LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return const Scaffold(
+            body: Center(
+              child: Text("No Route Defined!"),
+            ),
+          );
+        });
+    }
+  }
+}
---
+++ lib/view/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mvvmapp/utils/routes/routes_name.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InkWell(
+          onTap: () {
+            Navigator.pushNamed(context, RoutesName.home);
+          },
+          child: const Center(child: Text("Click"))),
+    );
+  }
+}
