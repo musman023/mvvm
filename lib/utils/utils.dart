1fe840771e8d7eea630efb08bdc3a28f91da78fb
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-27 23:24:18 +0500

    set till utils

---
+++ lib/utils/utils.dart
@@ -0,0 +1,38 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+
+class Utils {
+  static toastmessage(String message) {
+    Fluttertoast.showToast(msg: message);
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        borderRadius: BorderRadius.circular(10),
+        messageSize: 18,
+        forwardAnimationCurve: Curves.decelerate,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.all(15),
+        backgroundColor: Colors.red,
+        reverseAnimationCurve: Curves.easeInOut,
+        positionOffset: 20,
+        icon: const Icon(
+          Icons.error,
+          size: 28,
+          color: Colors.white,
+        ),
+        message: message,
+        duration: const Duration(seconds: 3),
+      )..show(context),
+    );
+  }
+
+  static snackBar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(backgroundColor: Colors.red, content: Text(message)));
+  }
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mvvmapp/utils/routes/routes_name.dart';
+import 'package:mvvmapp/utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,7 +14,10 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: InkWell(
           onTap: () {
-            Navigator.pushNamed(context, RoutesName.home);
+            Utils.flushBarErrorMessage('No internet connection', context);
+
+            Utils.snackBar('No internet connection', context);
+            Utils.toastmessage('No internet connection');
           },
           child: const Center(child: Text("Click"))),
     );
